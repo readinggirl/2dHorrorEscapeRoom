c89ed8730b9a7df8c7f002ee9109dbec6953d5fa
readinggirl <teresa.edinger@gmail.com>
2026-01-15 12:00:30 +0100

    added lighting, trying tests (no functionality)

--- AGENTS.md
+++ AGENTS.md
@@ -2,12 +2,13 @@
 - Project name: 2dEscapeRoom
 - Unity version: Unity 6000.2.10f1
 - Active scene:
+  - Name: Indoor
   - Tags:
     - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController, Pickup, Door
   - Layers:
     - Default, TransparentFX, Ignore Raycast, Water, UI, Pushable
 - Active game object:
-  - Name: Custom
+  - Name: Freeform Light 2D (5)
   - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/Tests/Tests/TestScript.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TestScript
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void TestScriptSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator TestScriptWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+
+    [Test(ExpectedResult = true)]
+    public bool TestTrue() {
+        return Mathf.IsPowerOfTwo(4);
+    }
+}
